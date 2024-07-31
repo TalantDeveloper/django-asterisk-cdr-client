9b3d00ce93c4acb2daa535388111b98a6e78a90c
Botirjon Ergashov <139768692+TalantDeveloper@users.noreply.github.com>
2024-07-31 10:46:25 +0500

    Update views.py

--- cdr/views.py
+++ cdr/views.py
@@ -7,9 +7,9 @@ from cdr.models import Cdr
 from datetime import date, datetime, timedelta
 from cdr.forms import CdrSearchForm
 
-#@staff_member_required
-def home(request):
 
+# @staff_member_required
+def home(request):
     if not request.GET.has_key('csrfmiddlewaretoken'):
         form = CdrSearchForm()
         registros = Cdr.objects.filter().using(request.database)
@@ -31,13 +31,13 @@ def home(request):
             disposition = form.cleaned_data.get('disposition', 'ALL')
             if disposition != 'ALL':
                 registros = registros.filter(disposition=disposition).using(request.database)
-            
-        
-    return render_to_response("cdr/index.html",
-                                {'registros':registros, 'form':form},
-                                context_instance=RequestContext(request))
 
-#@staff_member_required
+    return render_to_response("cdr/index.html",
+                              {'registros': registros, 'form': form},
+                              context_instance=RequestContext(request))
+
+
+# @staff_member_required
 def set_db(request, database):
     request.session['DB'] = database
     return HttpResponseRedirect(reverse('home'))
